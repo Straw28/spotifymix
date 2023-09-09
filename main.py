88a525853bdc6c9ecb6f7dc4e5c904cf5f1e444b
Straw28 <101749695+Straw28@users.noreply.github.com>
2023-09-08 23:06:31 -0500

    creating the react app

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def get_auth_header(token):
 def search_for_artist(token, artist_name):
     url = "https://api.spotify.com/v1/search"
     headers = get_auth_header(token)
-    query = f'?q={artist_name}&type=artist&limit=1'
+    query = f'?q={artist_name}&type=artist&limit=1' #spotify api already takes care of spaces and lowercase etc
 
     query_url = url + query
     result = get(query_url, headers=headers)
@@ -50,11 +50,24 @@ def search_for_artist(token, artist_name):
 
     return json_result[0]
 
+    
 token = get_token()
-result = search_for_artist(token, "ACDC")
+result = search_for_artist(token, "olivia rodrigo") 
 artist_id = result['id']
 print(artist_id)
 
-#search for an artist, get their top tracks, we need to get the id of an artist
-#search for an artist
-# get their id
+artist_1_name = input('Enter the first artist: ')
+artist_2_name  = input('Enter the second artist: ')
+
+token = get_token()
+result_1 = search_for_artist(token, artist_1_name) 
+artist_1_id = result_1['id']
+
+result_2 = search_for_artist(token, artist_2_name) 
+artist_2_id = result_2['id']
+
+print(f'id of first artist: {artist_1_id}')
+print(f'id of second artist: {artist_2_id}')
+
+
+#Give user option to view playlist first and shuffle to a new one, before creating it for them on spotify
